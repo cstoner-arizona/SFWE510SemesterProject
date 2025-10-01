daefa3828604f43a5b28943e31dcdf27ada6c165
cstoner-arizona <cstoner@arizona.edu>
2025-09-30 17:25:41 -0700

    fixed dep issues; config server works now.

--- configserver/src/main/java/org/skate/configserver/ConfigurationServerApplication.java
+++ configserver/src/main/java/org/skate/configserver/ConfigurationServerApplication.java
@@ -1,10 +1,10 @@
 package org.skate.configserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootConfiguration
+@SpringBootApplication
 @EnableConfigServer
 public class ConfigurationServerApplication {
     public static void main(String[] args) {
